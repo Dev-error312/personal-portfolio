d3d42a997fb4b6404b56d1af69c14ab79a4fe336
Suyog Adhikari <suyogadhiakri@gmail.com>
2022-07-26 15:06:20 +0545

    Added mailchimp function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Banner } from './components/Banner'
 import { Skills } from './components/Skills'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
+import { Newsletter } from './components/Newsletter'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
