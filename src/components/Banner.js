e7b97aeb0cc4bcbd6f8aad4f7863b9f42df8624d
Suyog Adhikari <suyogadhiakri@gmail.com>
2022-07-26 16:40:13 +0545

    Finished Footer

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { ArrowRightCircle } from 'react-bootstrap-icons'
 import headerImg from '../assets/img/header-img.svg'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -50,12 +52,17 @@ export const Banner = () => {
             <Container>
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
-                        <span className='tagline'>
-                            Welcome to my Portfolio
-                        </span>
-                        <h1>{`Hi I'm Suyog `}<span className='wrap'>{text}</span></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quia vitae quos illo fugit accusamus vel, debitis, qui pariatur libero, voluptas at soluta rem ab beatae reiciendis quibusdam minus explicabo?</p>
-                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                        <TrackVisibility>
+                        {({ isVisible }) => 
+                          <div className={isVisible ? 'animate__animated animate__fadeIn' : ''}>
+                            <span className='tagline'>
+                                Welcome to my Portfolio
+                            </span>
+                            <h1>{`Hi I'm Suyog `}<span className='wrap'>{text}</span></h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quia vitae quos illo fugit accusamus vel, debitis, qui pariatur libero, voluptas at soluta rem ab beatae reiciendis quibusdam minus explicabo?</p>
+                            <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                          </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img" />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import colorSharp2 from '../assets/img/color-sharp2.png'
 import projImg1 from '../assets/img/project-img1.png'
 import projImg2 from '../assets/img/project-img2.png'
 import projImg3 from '../assets/img/project-img3.png'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
 
 export const Projects = () => {
 
@@ -44,10 +46,17 @@ export const Projects = () => {
             <Container>
                 <Row>
                     <Col>
-                        <h2>
-                            Projects
-                        </h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, fugit. Aliquam, voluptatibus perspiciatis expedita voluptas quasi veritatis harum doloribus sint mollitia perferendis placeat veniam, eveniet officia nisi vel iste praesentium?</p>
+                    <TrackVisibility>
+                    {({ isVisible }) =>
+                        <div className={isVisible ? 'animate__animated animate__pulse' : ''}>
+                            <h2>
+                                Projects
+                            </h2>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, fugit. Aliquam, voluptatibus perspiciatis expedita voluptas quasi veritatis harum doloribus sint mollitia perferendis placeat veniam, eveniet officia nisi vel iste praesentium?</p>
+                        </div>
+                    }
+                    </TrackVisibility>
+                        
                         <Tab.Container id='projects-tabs' defaultActiveKey='first'>
                         <Nav variant='pills' className='nav-pills mb-5 justify-content-center align-items-center' id='pills-tab'>
                             <Nav.Item>
